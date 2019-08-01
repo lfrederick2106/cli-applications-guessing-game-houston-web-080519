971bb9569cac4007a6112e5fa2750c4b411a706e
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-08-01 05:02:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,5 @@
 def run_guessing_game
-  random_number = rand(1..6).to_s
+  random_number = rand(1...6).to_s
   puts "Guess your number!"
   user_response = gets.chomp
   if user_response == random_number
