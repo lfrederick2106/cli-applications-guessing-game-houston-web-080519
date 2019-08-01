56922e3fc2234dc2c631114f2f443b69505051f3
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-08-01 04:35:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,10 +4,8 @@ def run_guessing_game
   user_response = gets
   if user_response == random_number
     puts "You guessed the correct number!"
-  end
   elsif "exit"
     puts "Goodbye!"
-  end
   else
   #& user_response.is_a? Integer
     puts "Sorry! The computer guessed #{random_number}."
