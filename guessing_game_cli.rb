a63be9151753319047a0c487be4bfc07224f9625
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-08-01 05:11:59 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,14 @@
-# Code your solution here!
+def run_guessing_game
+  random_number = rand(1..6).to_s
+  puts "Guess your number!"
+  user_response = gets.chomp
+  if user_response == random_number
+    print "You guessed the correct number!"
+  elsif user_response == "exit"
+    print "Goodbye!"
+  else
+    print "Sorry! The computer guessed #{random_number}."
+  end
+end
+
+puts run_guessing_game
